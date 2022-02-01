461763bb894661d14c6f5fc41ac7906a5ce965b7
nikhil2004 <93114389+nikhilk2004@users.noreply.github.com>
2022-02-01 09:23:46 +0000

    complete

---
+++ p5final.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("enter any number :");
+  scanf("%d",&n);
+  return n;
+}
+float my_sqrt(int n)
+{
+  double start,end,mid;
+  start=0,end=n;
+  while((end-start)>=0.00001)
+  {
+    mid=(start+end)/2;
+    if(mid*mid<n)
+    start=mid;
+    if (mid*mid>=n)
+    end=mid;
+  }
+  return mid;
+}
+void output(float sqrt_result)
+{
+  printf("sq root is %f",sqrt_result);
+}
+int main()
+{
+  int a;
+  float b;
+  a=input();
+  b=my_sqrt(a);
+  output(b);
+  return 0;
+}
